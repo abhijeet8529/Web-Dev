0c84e00ce1c32da232b10d8e8234dfbdace0938b
abhijeet <abhijeetsarkar2002@gmail.comcd>
2024-02-15 22:53:46 +0530

    feb

---
+++ Project-practice/Stone-paper-scissor/script.js
@@ -0,0 +1,75 @@
+let user = 0;
+let comp = 0;
+const choices = document.querySelectorAll(".c");
+const msg = document.querySelector(".turn");
+const userscore = document.querySelector("#userscore");
+const compscore = document.querySelector("#compscore");
+const btn = document.querySelector('.btn');
+
+
+const resetgame = () =>{
+	user = 0;
+	comp = 0;
+	userscore.innerText = user;
+	compscore.innerText = comp;
+	msg.innerText = 'Play your move';
+	msg.style.backgroundColor = "white"
+
+}
+
+const showWinner = (userwin, choiceID, computer) => {
+	if (userwin) {
+		user++;
+		userscore.innerText = user;
+		msg.innerText = `You win! , ${choiceID} beats ${computer}`;
+		msg.style.backgroundColor = "#B6EEA6";
+	} else {
+		comp++;
+		compscore.innerText = comp;
+		console.log("You lose, better luck next time");
+		msg.innerText = `You lose, ${computer} beats ${choiceID}`;
+		msg.style.backgroundColor = "#FE5F55";
+	}
+};
+
+const compchoice = () => {
+	const compoptions = ["rock", "paper", "scissors"];
+	const randidx = Math.floor(Math.random() * 3);
+	return compoptions[randidx];
+};
+
+const drawgame = () => {
+	msg.innerText = "game was draw";
+	msg.style.backgroundColor = "white";
+};
+
+const playgame = (choiceID) => {
+	// console.log("user choiced =", choiceID);
+	//generate choice choice
+	const computer = compchoice();
+	console.log("computer choice : ", computer);
+
+	if (choiceID === computer) {
+		drawgame();
+	} else {
+		let userwin = true;
+		if (choiceID === "rock") {
+			userwin = computer === "paper" ? false : true;
+		} else if (choiceID === "paper") {
+			userwin = computer === "scissors" ? false : true;
+		} else {
+			userwin = computer === "rock" ? false : true;
+		}
+		showWinner(userwin, choiceID, computer);
+	}
+};
+
+choices.forEach((choice) => {
+	choice.addEventListener("click", () => {
+		const choiceID = choice.getAttribute("id");
+		console.log("clicked", choiceID);
+		playgame(choiceID);
+	});
+});
+
+btn.addEventListener('click', resetgame);
