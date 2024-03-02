7df5787c18431e8db64a8f59a7f5cb8cbac0f879
abhijeet <abhijeetsarkar2002@gmail.comcd>
2024-03-02 11:40:57 +0530

    feb

--- JavaScriptQprac/JSques4.js
+++ JavaScriptQprac/JSques4.js
@@ -18,3 +18,50 @@
 // let a = ['flower', 'flose', 'flare']
 
 // console.log(prefix(a))
+
+//2sum
+// function twosum(nums, target) {
+// 	let maps = new Map();
+// 	for (let i = 0; i < nums.length; i++) {
+// 		const pair = target - nums[i];
+// 		if (maps.has(pair)) return [maps.get(pair), i];
+// 		maps.set(nums[i], i);
+// 	}
+// }
+
+// nums = [2, 7, 4,8, 54, 56];
+// console.log(twosum(nums, 12));
+
+//3sum
+// let nums = [-1, 0, 1, 2, -1, -4];
+
+// function threesum(nums) {
+// 	if (nums === null || nums.length < 3) return [];
+// 	nums.sort((a, b) => a - b);
+// 	let result = [];
+
+// 	for (let i = 0; i < nums.length - 2; i++) {
+// 		if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+// 			let left = i + 1;
+// 			let right = nums.length - 1;
+
+// 			while (left < right) {
+// 				let sum = nums[i] + nums[right] + nums[left];
+
+// 				if (sum === 0) {
+// 					result.push([nums[i], nums[left], nums[right]]);
+
+// 					left++;
+// 					right--;
+// 				} else if (sum < 0) {
+// 					left++;
+// 				} else {
+// 					right--;
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return result;
+// }
+// console.log(threesum(nums));
+
